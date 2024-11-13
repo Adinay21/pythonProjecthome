de5ec5f16c2fb31acd64c42b40b0efafdbd38563
Adinay21 <adinayken@gmail.com>
2024-11-14 02:12:24 +0600

    hw4

--- handlers/review_dialog.py
+++ handlers/review_dialog.py
@@ -30,6 +30,10 @@ async def process_name(message: types.Message, state: FSMContext):
 
 @review_router.message(RestourantReview.phone_number)
 async def process_age(message: types.Message, state: FSMContext):
+    number = message.text
+    if number.isdigit():
+        await message.answer("Вводите только цифры")
+        return
     await state.update_data(phone_number=message.text)
     await state.set_state(RestourantReview.instagram_username)
     await message.answer("Укажите свой инстаграм:")
@@ -44,19 +48,48 @@ async def process_gender(message: types.Message, state: FSMContext):
 async def process_gender(message: types.Message, state: FSMContext):
     await state.update_data(visit_date=message.text)
     await state.set_state(RestourantReview.food_rating)
-    await message.answer("Как оцениваете качество еды?")
+    kb = types.ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                types.KeyboardButton(text="1"),
+                types.KeyboardButton(text="2"),
+                types.KeyboardButton(text="3"),
+                types.KeyboardButton(text="4"),
+                types.KeyboardButton(text="5")
+            ]
+        ]
+    )
+    await message.answer("Как оцениваете качество еды?", reply_markup=kb)
 
-@review_router.message(RestourantReview.food_rating)
+@review_router.message(RestourantReview.food_rating, F.text.in_(["1", "2", "3", "4", "5"]))
 async def process_gender(message: types.Message, state: FSMContext):
+    kb = types.ReplyKeyboardRemove()
     await state.update_data(food_rating=message.text)
     await state.set_state(RestourantReview.cleanliness_rating)
-    await message.answer("Как оцениваете чистоту заведения?")
+    kb = types.ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                types.KeyboardButton(text="1"),
+                types.KeyboardButton(text="2"),
+                types.KeyboardButton(text="3"),
+                types.KeyboardButton(text="4"),
+                types.KeyboardButton(text="5"),
+                types.KeyboardButton(text="6"),
+                types.KeyboardButton(text="7"),
+                types.KeyboardButton(text="8"),
+                types.KeyboardButton(text="9"),
+                types.KeyboardButton(text="10")
+            ]
+        ]
+    )
+    await message.answer("Как оцениваете чистоту заведения?", reply_markup=kb)
 
-@review_router.message(RestourantReview.cleanliness_rating)
+@review_router.message(RestourantReview.cleanliness_rating, F.text.in_(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]))
 async def process_gender(message: types.Message, state: FSMContext):
+    kb = types.ReplyKeyboardRemove()
     await state.update_data(cleanliness_rating=message.text)
     await state.set_state(RestourantReview.extra_comments)
-    await message.answer("Дополнительные жалобы и комментарии:")
+    await message.answer("Дополнительные жалобы и комментарии:", reply_markup=kb)
 
 @review_router.message(RestourantReview.extra_comments)
 async def process_genre(message: types.Message, state: FSMContext):
